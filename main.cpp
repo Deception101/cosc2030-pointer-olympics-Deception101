87042f54f47496da5f418ac552dfca14753376b1
Deception101 <kytenjohnson@sheridan.edu>
2026-09-04 04:43:45 +0000

    imported week02 and got the number of athletes

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip> // For std::fixed and std::setprecision
+
+using namespace std; 
+
+// Function prototypes
+void sortScores(double* scores, int numScores);
+double calculateAverage(double* scores, int numScores);
+void awardMedals(double* scores, int numScores);
+void displayResults(double* scores, int numScores, double average);
+
+int main() {
+    int numAthletes = 0;
+    // Get number of scores from user
+    cout << "Enter the number of athletes: ";
+    cin >> numAthletes;
+
+    // Dynamically allocate array for scores
+
+    // Get scores from user
+
+    // Sort scores
+
+    // Calculate average score
+
+    // Award medals
+
+    // Display results
+
+    // Deallocate memory for scores array
+
+    return 0;
+}
